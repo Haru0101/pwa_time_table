738839f2508abaaca3a12e0a0e18646c35b8e39e
Haruki Okuda <sunny_smile_0v0@yahoo.co.jp>
2020-04-02 21:36:41 +0900

    本番用のvueに切り替え

---
+++ js/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+<head>
+  <meta charset="utf-8" />
+  <meta
+    name="viewport"
+    content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, viewport-fit=cover"
+  />
+  <!-- 拡大縮小禁止 = maximum, minimum-scale=1 あとは画面幅に縦横を合わせるなどの設定-->
+  <meta name="apple-mobile-web-app-title" content="TFM1999" />
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <link rel="apple-touch-icon" href="images/icon.png" />
+  <link rel="apple-touch-startup-image" href="images/big-icon.png" />
+  <!-- 上4行はiOS用のアプリ化のタグ -->
+  <script src="js/jquery.min.js"></script>
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+  <script src="js/bootstrap.min.js"></script>
+  <link
+    rel="stylesheet"
+    href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+    crossorigin="anonymous"
+  />
+  <link rel="stylesheet" href="css/common.css" />
+  <title>TFM 1999</title>
+</head>
+</template>
