15018cef36e8ae7a445c3ad49ca09b542acb583b
Haruki Okuda <sunny_smile_0v0@yahoo.co.jp>
2020-04-02 21:53:44 +0900

    パスの修正

--- js/components/Header.vue
+++ js/components/Header.vue
@@ -11,16 +11,16 @@
   <link rel="apple-touch-icon" href="images/icon.png" />
   <link rel="apple-touch-startup-image" href="images/big-icon.png" />
   <!-- 上4行はiOS用のアプリ化のタグ -->
-  <script src="js/jquery.min.js"></script>
-  <link rel="stylesheet" href="css/bootstrap.min.css" />
-  <script src="js/bootstrap.min.js"></script>
+  <script src="../../js/jquery.min.js"></script>
+  <link rel="stylesheet" href="../../css/bootstrap.min.css" />
+  <script src="../../js/bootstrap.min.js"></script>
   <link
     rel="stylesheet"
     href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
     integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
     crossorigin="anonymous"
   />
-  <link rel="stylesheet" href="css/common.css" />
+  <link rel="stylesheet" href="../../css/common.css" />
   <title>TFM 1999</title>
 </head>
 </template>
