904fe626a6c7348b3fca9ee865ab1f7e905f37e6
Haruki Okuda <sunny_smile_0v0@yahoo.co.jp>
2020-04-05 15:40:12 +0900

    戻し

--- js/main.js
+++ js/main.js
@@ -43,8 +43,8 @@ $(function () {
 
     });
     //初期設定
-    getPage("index.html");
-    var lastpage = "index.html";
+    getPage("1.html");
+    var lastpage = "1.html";
 
     //ページを取得してくる
     function getPage(elm) {
